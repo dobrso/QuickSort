e6b812fa2aa4ee514ba5d63a700a66dc0d880c67
dobrso <ainurakhmedshin@gmail.com>
2025-04-25 21:20:25 +0300

    qsort implementation

--- quickSort.py
+++ quickSort.py
@@ -1,1 +1,21 @@
-def qsort():
+import random
+from typing import List
+
+def qsort(arr: List[int]) -> List[int]:
+    if len(arr) <= 1 or arr == sorted(arr):
+        return arr
+
+    pivot = random.choice(arr)
+    left = []
+    middle = []
+    right = []
+
+    for element in arr:
+        if element < pivot:
+            left.append(element)
+        elif element > pivot:
+            right.append(element)
+        else:
+            middle.append(pivot)
+
+    return qsort(left) + middle + qsort(right)
