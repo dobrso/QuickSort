1da569f7fcc22ccd553d25eb2ddfe86629f5b888
dobrso <ainurakhmedshin@gmail.com>
2025-04-25 21:15:41 +0300

    measure time function implementation

---
+++ calculationAndCreation.py
@@ -0,0 +1,23 @@
+import time
+import numpy as np
+from typing import Callable, Any
+
+def measureTime(func: Callable[..., Any], *args: Any, repeats: int) -> np.ndarray:
+    times = []
+
+    for _ in range(repeats):
+        timeStart = time.perf_counter()
+        func(*args)
+        timeEnd = time.perf_counter()
+
+        timeDiff = timeEnd - timeStart
+        times.append(timeDiff)
+
+    meanTime = np.mean(times)
+    return meanTime
+
+def countIterations():
+    ...
+
+def generateInputData():
+    ...
